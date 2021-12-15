1621b5cb4bfddb073b9b6638889cabfb7f0fb3f6
Sander Allert <sander.allert@lytix.be>
2021-12-15 14:23:36 +0100

    Fixed URL

--- Visual Studio Project/VIEW_DWH_V_Demo_Absences.sql
+++ Visual Studio Project/VIEW_DWH_V_Demo_Absences.sql
@@ -1,8 +1,5 @@
 CREATE OR ALTER VIEW DWH.V_Demo_Absences AS
 SELECT
     TOP 100 *
-FROM
-    OPENROWSET(
-        BULK 'https://tgdataplatformdev.dfs.core.windows.net/datalakedev/STAGING/ATPS/ABSENCES/ABSENCES.parquet',
-        FORMAT = 'PARQUET'
-    ) AS [result]
+FROM [dbo].[V_Demo2_Absences] 
+CROSS JOIN [DWH].[V_Demo2_Absences] 
